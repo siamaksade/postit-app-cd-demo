ccf1eb59c663bac17fa7cd067a051e253e59d2db
Siamak Sadeghianfar <ssadeghi@redhat.com>
2020-04-06 15:44:59 -0400

    updated

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
 declare cicd_prj=post-app-cicd 
 declare dev_prj=post-app-sun
 
-oc new-project $cicd_prj 
+oc new-project $cicd_prj || true
 oc policy add-role-to-user edit system:serviceaccount:$cicd_prj:pipeline -n $dev_prj
 
 oc apply -f cd -n $cicd_prj
@@ -13,6 +13,8 @@ oc apply -f tasks -n $cicd_prj
 oc apply -f pipelines -n $cicd_prj
 oc apply -f config/maven-configmap.yaml -n $cicd_prj
 
+oc apply -f triggers/github-pr-triggerbinding.yaml -n $cicd_prj
 oc apply -f triggers/github-triggerbinding.yaml -n $cicd_prj
-oc apply -f triggers/triggertemplate.yaml -n $cicd_prj
-oc apply -f triggers -n $cicd_prj
+oc apply -f triggers/triggertemplate-commit.yaml -n $cicd_prj
+oc apply -f triggers/triggertemplate-pr.yaml -n $cicd_prj
+oc apply -f triggers/eventlistener.yaml -n $cicd_prj
